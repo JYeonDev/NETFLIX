60f757fb67fb2003e8181f1c8bbf770329929130
JYeonDev <whddus4837@gmail.com>
2022-06-13 22:17:34 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import Router from "./routes/router";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -74,14 +68,10 @@ li {
 `;
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ThemeProvider>
+      <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
