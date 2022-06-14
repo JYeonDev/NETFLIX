f04557a0889fa68ce62c3802586598b38e545b1b
JYeonDev <whddus4837@gmail.com>
2022-06-15 01:06:28 +0900

    Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 function ToDoList() {
-  return <div></div>;
+  const { register, watch, handleSubmit } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("Email", { required: true, minLength: 10 })}
+          placeholder='Email'
+        />
+        <input {...(register("1"), { required: true })} placeholder='Email' />
+        <input {...(register("2"), { required: true })} placeholder='Email' />
+        <input {...(register("3"), { required: true })} placeholder='Email' />
+        <input {...(register("4"), { required: true })} placeholder='Email' />
+        <input {...(register("5"), { required: true })} placeholder='Email' />
+        <input {...(register("6"), { required: true })} placeholder='Email' />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
+
+// function ToDoList() {
+//   const [todo, setTodo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(todo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder='Write a to do' />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default ToDoList;
