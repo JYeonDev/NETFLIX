6979268d931b933174a5d02a622ff2538d1ab303
JYeonDev <whddus4837@gmail.com>
2022-06-20 19:02:03 +0900

    Reordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,84 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
+import { toDoState } from "./atoms";
+
+const Wrraper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const copyToDos = [...oldToDos];
+      copyToDos.splice(source.index, 1);
+      copyToDos.splice(destination?.index, 0, draggableId);
+      return copyToDos;
+    });
   };
-
   return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type='number'
-        placeholder='Minutes'
-      />
-      <input value={hours} type='number' placeholder='Hours' />
-    </div>
+    <Wrraper>
+      <Boards>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div>
+            <Droppable droppableId='one'>
+              {(magic) => (
+                <Board ref={magic.innerRef} {...magic.droppableProps}>
+                  {toDos.map((toDo, index) => (
+                    <Draggable key={toDo} draggableId={toDo} index={index}>
+                      {(magic) => (
+                        <Card
+                          ref={magic.innerRef}
+                          {...magic.draggableProps}
+                          {...magic.dragHandleProps}
+                        >
+                          {toDo}
+                        </Card>
+                      )}
+                    </Draggable>
+                  ))}
+                  {magic.placeholder}
+                </Board>
+              )}
+            </Droppable>
+          </div>
+        </DragDropContext>
+      </Boards>
+    </Wrraper>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "black",
-  accentColor: "#44bd32",
+  bgColor: "#50BCDF",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
